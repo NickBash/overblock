caec33a9d67078b6a33a5531249fc0f21cfe05ef
NickBash <nikitabash7@gmail.com>
2019-02-24 23:52:34 +0500

    OB beta 0.3.2

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <div id="app">
-    <div class="graf">
-      <Doughnut :chart-data="this.datacoll" :height="400" :options="this.options"></Doughnut>
-      <p v-if="loaded"> {{ this.warning }} из {{ this.sum }} считают этот сайт опасным</p>
+    <div v-if="!loaded" class="cssload-thecube">
+      <div class="spinner">
+        <div class="rect1"></div>
+        <div class="rect2"></div>
+        <div class="rect3"></div>
+        <div class="rect4"></div>
+        <div class="rect5"></div>
+      </div>
     </div>
+    <div v-if="loaded" class="graf">
+      <Doughnut :chart-data="this.datacoll" :height="400" :options="this.options"></Doughnut>
+      <p> {{ this.warning }} из {{ this.sum }} источников считают этот сайт опасным</p>
+    </div>
+
     <input type="text" v-model="addres">
-    <button @click="get">Отправить</button>
+    <button @click="getUrl">Отправить</button>
   </div>
 </template>
 
@@ -18,24 +28,13 @@ export default {
   name: 'app',
   data () {
     return {
-      kek: 'https://learn.javascript.ru/string',
       scanUrl: '',
-      addres: '',
       clean: 0,
       unrated: 0,
       warning: 0,
       sum: null,
       loaded: false,
-      datacoll: {
-        labels: ["Нет данных"],
-        datasets: [{
-          data: [100],
-          backgroundColor: [
-            'rgba(71, 74, 81, 0.5)',
-          ],
-          borderWidth: 0,
-        }]
-      },
+      datacoll: '',
       options: {
         responsive: true,
         maintainAspectRatio: true,
@@ -61,23 +60,22 @@ export default {
     }
   },
   methods: {
-    clearUrl() {
-      let non = this.kek.indexOf('/', kek.indexOf('/') + 2) + 1;
-      let str = this.kek.substring(0, non);
-      console.log(str);
-    },
-    getUrl(url) {
-      if (url) {
-        axios.post('http://localhost/api/post-url-scan', {
-          url: url,
+    // clearUrl() {
+    //   let non = this.kek.indexOf('/', kek.indexOf('/') + 2) + 1;
+    //   let str = this.kek.substring(0, non);
+    //   console.log(str);
+    // },
+    getUrl() {
+        axios.post('http://ovapi.ovd.su/api/post-url-scan', {
+          url: 'https://learn.javascript.ru/string',
           apikey: '5ca8277fafc89da750fe37e6aa5640f8de23226b9c35592f74f876be6c020366',
           crossDomain: true,
         })
           .then(response => {
             let data = JSON.parse(response.data);
             if (data.response_code == 1) {
-              axios.post('http://localhost/api/post-url-report', {
-                url: url,
+              axios.post('http://ovapi.ovd.su/api/post-url-report', {
+                url: 'https://learn.javascript.ru/string',
                 apikey: '5ca8277fafc89da750fe37e6aa5640f8de23226b9c35592f74f876be6c020366',
                 crossDomain: true,
               })
@@ -90,7 +88,7 @@ export default {
                     datasets: [{
                       data: [response.data.clean, response.data.warning, response.data.unrated],
                       backgroundColor: [
-                        'rgba(75, 192, 192, 0.8)',
+                        'rgba(66, 184, 131, 0.8)',
                         'rgba(255, 99, 132, 0.8)',
                         'rgba(71, 74, 81, 0.8)',
                       ],
@@ -106,14 +104,8 @@ export default {
           .catch(function (error) {
             console.log(error);
           })
-      } else {
-        console.log("no -> ")
-      }
 
     }
-  },
-  mounted() {
-
   },
   created() {
     // chrome.tabs.getSelected(null, (tab) => {
@@ -168,10 +160,6 @@ a {
   color: #42b983;
 }
 
-.logo {
-  width: 120px;
-}
-
 #app {
   margin-top: 20px;
   min-width: 280px;
@@ -182,11 +170,57 @@ a {
   padding: 0 30px;
 }
 
-.card-panel p{
-  font-size: 40px;
-  margin: 0;
-  font-weight: 700;
-  color: white;
+.spinner {
+  margin: 100px auto;
+  width: 50px;
+  height: 40px;
+  text-align: center;
+  font-size: 10px;
+}
+
+.spinner > div {
+  background-color: #42b883;
+  height: 100%;
+  width: 6px;
+  display: inline-block;
+
+  -webkit-animation: sk-stretchdelay 1.2s infinite ease-in-out;
+  animation: sk-stretchdelay 1.2s infinite ease-in-out;
+}
+
+.spinner .rect2 {
+  -webkit-animation-delay: -1.1s;
+  animation-delay: -1.1s;
+}
+
+.spinner .rect3 {
+  -webkit-animation-delay: -1.0s;
+  animation-delay: -1.0s;
+}
+
+.spinner .rect4 {
+  -webkit-animation-delay: -0.9s;
+  animation-delay: -0.9s;
+}
+
+.spinner .rect5 {
+  -webkit-animation-delay: -0.8s;
+  animation-delay: -0.8s;
+}
+
+@-webkit-keyframes sk-stretchdelay {
+  0%, 40%, 100% { -webkit-transform: scaleY(0.4) }
+  20% { -webkit-transform: scaleY(1.0) }
+}
+
+@keyframes sk-stretchdelay {
+  0%, 40%, 100% {
+    transform: scaleY(0.4);
+    -webkit-transform: scaleY(0.4);
+  }  20% {
+       transform: scaleY(1.0);
+       -webkit-transform: scaleY(1.0);
+     }
 }
 
 
